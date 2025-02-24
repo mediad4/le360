893af8f09e0a9a8296105643fca9ce5e4d0fb1fd
SalySoum <salysoumahoro92@gmail.com>
2025-02-24 21:26:17 +0100

    Add files via upload

---
+++ Animal.py
@@ -0,0 +1,8 @@
+class Animal:
+    def __init__(self,couleur,poids):
+        self.couleur = couleur
+        self.poids = poids
+    def afficherDureeVie(self):
+        print("Pas determinee")
+    def crier(self):
+        print("pas determinee")
